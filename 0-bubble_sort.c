bec6dd4c88eba695f50bc3204123b9f11f339495
Dahesey <dahesey@gmail.com>
2023-11-21 23:13:52 +0000

    A function to sort elements using the bubble sort method

---
+++ 0-bubble_sort.c
@@ -0,0 +1,46 @@
+/**
+ * swap - A function to swap out address
+ *
+ * @a: The first integer to be swapped after comparism
+ *
+ * @b: The second integer to be swapped after comparism
+ */
+
+#include "sort.h"
+
+void swap(int *a, int *b)
+{
+    int hold;
+
+    hold = *a;
+    *a = *b;
+    *b = hold;
+}
+
+/**
+ * bubble_sort - A function to sort elements using the bubble sort method
+ *
+ * @array: Array of elements
+ *
+ * @size: Size of the array
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	int i, j;
+
+    /* Loop to control the number of passes */
+	for (i = 1; i <= size; i++)
+	{
+        /* Loop to control the number of comparisons per pass */
+		for (j = 0; j < size - 1; j++)
+		{
+            /* Swap if current element > next */
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
+			}
+		}
+	}
+}
